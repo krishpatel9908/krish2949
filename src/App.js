a0d90a925d8b3e40903780626d706a14aa92348a
krish2949 <krishpatel2949@gmail.com>
2022-11-18 15:38:11 +0530

    hello

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+      <h1>
+        Krish patel
+      </h1>
+      <h1>
+        Front End Development
+      </h1>
+      <h1>
+      Lorem ipsum is typically a corrupted version of De finibus bonorum et malorum, a 1st-century BC text by the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical and improper Latin.
+      </h1>
+    </div>
+  );
+}
+
+export default App;
